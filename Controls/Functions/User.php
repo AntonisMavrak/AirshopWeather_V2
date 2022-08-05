2e1e1c5c49d9d23604491eccbf262a8e4f3af77a
StellaBkl <57630144+StellaBkl@users.noreply.github.com>
2022-08-05 03:07:30 +0300

    added User.php updated init.php index.php

---
+++ Controls/Functions/User.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Controls\Functions;
+
+use Controls\Database\Database;
+
+class User
+{
+    use Database;
+
+    public function saveSearch($input)
+    {
+        // TODO connect to DB
+        $collection = $this->mongo('user');
+
+        // TODO find user
+        $match = [
+            'name' => 'Stella Boukla'
+        ];
+
+        //TODO insert table
+        $insert = ['$push' => [
+            $input['flag'] => $input['data']
+        ]
+        ];
+        $options = [
+
+        ];
+        //TODO delete table
+        $delete=['$pop'=>
+            [$input['flag']=>-1]
+        ];
+
+        $pagesData = $collection->findOne($match, $options);
+
+        if (count($pagesData[$input['flag']]) >= 10) {
+            $collection->updateOne($match, $delete);
+        }
+        $collection->updateOne($match, $insert);
+
+        $this->showSearch($input['flag']);
+    }
+
+    private function showSearch($input)
+    {
+        // TODO connect to DB
+        $collection = $this->mongo('user');
+
+        // TODO check if page with name exists
+        $match = [
+            'name' => 'Stella Boukla'
+        ];
+        $options = [
+
+        ];
+        $pagesData = $collection->findOne($match, $options);
+
+        if ($pagesData !== null) {
+//            for($i=1;$i<count($pagesData);$i++){
+//                echo '<br>'.$pagesData[$input]['search'.$i].'<br>';
+//            }
+            foreach ($pagesData[$input] as $pageData) {
+                echo '<br>' . $pageData . '<br>';
+            }
+            return true;
+        }
+        return false;
+
+
+    }
+
+}
--- Controls/Functions/init.php
+++ Controls/Functions/init.php
@@ -6,53 +6,58 @@ use Controls\Router\routing;
 
 class init
 {
-    public function load(){
-        $router= new routing();
-        $input=file_get_contents('php://input');
-        $request=  $_SERVER['REQUEST_URI'] === ($_SERVER['APP_BASE'] ?? '') ? '/index.html' : $_SERVER['REQUEST_URI'];
+    public function load()
+    {
+        $router = new routing();
+        $input = file_get_contents('php://input');
+        $request = $_SERVER['REQUEST_URI'] === ($_SERVER['APP_BASE'] ?? '') ? '/index.html' : $_SERVER['REQUEST_URI'];
         return !empty($input) ? $this->runCommand($input) : $router->routing($request);
     }
+
 //prepi na gini sanitaze to uri kai ta data;
-    public function runCommand($input){
-        $input=json_decode($input,true);
-        switch ($input['flag']){
-            case 'register':register($input);
+    public function runCommand($input)
+    {
+        $user= new User();
+        $input = json_decode($input, true);
+       // print_r($input);
+        switch ($input['flag']) {
+            case 'register':
+                //register($input);
                 break;
-            case'login':login($input);
+            case'login':
+                //login($input);
                 break;
-            case'history':saveSearch($input);
+            case'history':
+                $user->saveSearch($input);
                 break;
-            case 'saved_data':handleData($input);
+            case 'saved_data':
+                //handleData($input);
                 break;
-            default: return false;
-
-
+            default:
+                return false;
         }
-
-        var_dump($input);
-//        return$input;
 
     }
 
-    public function run():string
+    public function run(): string
     {
-        $data=[
-            'h1'=>'My First Heading',
-            'p1'=>'Myfirst paragraph'
+        $data = [
+            'h1' => 'My First Heading',
+            'p1' => 'Myfirst paragraph'
         ];
-        $dummyArray=[
-            '{{language}}'=>'en',
-            '{{title}}'=>'<title>PageTitle</title>',
-            '{{meta}}'=>'',
-            '{{css}}'=>'',
-            '{{page}}'=>'index',
-            '{{data}}'=>json_encode($data,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT),
-            '{{js}}'=>'<script type="module" src="Assets/development/js/dexie.mjs"></script><script type="module" src="Assets/development/js/main.js"></script>'
+        $dummyArray = [
+            '{{language}}' => 'en',
+            '{{title}}' => '<title>PageTitle</title>',
+            '{{meta}}' => '',
+            '{{css}}' => '',
+            '{{page}}' => 'index',
+            '{{data}}' => json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT),
+            '{{js}}' => '<script type="module" src="Assets/development/js/dexie.mjs"></script><script type="module" src="Assets/development/js/main.js"></script>'
         ];
-        $bluePrint=file_get_contents('Views/htmlBluePrint.tpl');
-        $html=strtr($bluePrint,$dummyArray);
+        $bluePrint = file_get_contents('Views/htmlBluePrint.tpl');
+        $html = strtr($bluePrint, $dummyArray);
         return $html;
 
- }
+    }
 
 }
