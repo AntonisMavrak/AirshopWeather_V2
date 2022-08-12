c7bba5c5e71df16e083c775813067286079a0216
StellaBkl <57630144+StellaBkl@users.noreply.github.com>
2022-08-12 12:02:00 +0300

    updated api.js

--- Assets/Development/Workers/api.js
+++ Assets/Development/Workers/api.js
@@ -1,70 +1,96 @@
 onmessage = (event) => {
     console.log('Worker Received Message');
 
- switch ('forecast'){
-     case 'weather': getWeather().then((response) => {
-         postMessage(JSON.stringify(response));
-     });
-        break;
-     case 'airPollution':airPollution().then((response) => {
-         postMessage(JSON.stringify(response));
-     });
-        break;
-     case'forecast': weatherForecast().then((response) => {
-         postMessage(JSON.stringify(response));
-     })
-         break;
-     default: postMessage('ola lathos');
 
 
+    switch ('forecast') {
+        case 'weather':
+            getWeather().then((response) => {
+                postMessage(JSON.stringify(response));
+            });
+            break;
+        case 'airPollution':
+            airPollution().then((response) => {
+                postMessage(JSON.stringify(response));
+            });
+            break;
+        case'forecast':
+            weatherForecast().then((response) => {
+                postMessage(JSON.stringify(response));
+            })
+            break;
+        default:
+            postMessage('ola lathos');
 
- }}
-let getWeather=async ()=>{
-    let city_select='Athens';
-    let key='80d2ff5f959352f4319d73dc1f0171ce';
-    return  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_select},eng&lang=eng&units=metric&appid=${key}`,
-        {method: 'GET'}
-    )
-        .then(response => {
-            return response.json();
-        })
-        .catch((error)=>{
-            return error;
+
+    }
+}
+
+
+    let getWeather = async () => {
+        let city_select = 'Athens';
+        let key = '80d2ff5f959352f4319d73dc1f0171ce';
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_select},eng&lang=eng&units=metric&appid=${key}`,
+            {method: 'GET'}
+        )
+            .then(response => {
+                return response.json();
+            })
+            .catch((error) => {
+                return error;
             });
 
 
-}
+    }
 
-let airPollution=async ()=>{
-    let lat=37.983810;
-    let lon=37.983810;
-    let key='80d2ff5f959352f4319d73dc1f0171ce';
-    return  fetch(`https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${key}`,
-        {method: 'GET'}
-    )
-        .then(response => {
-            return response.json();
-        })
-        .catch((error)=>{
-            return error;
-        });
-
-
-}
-
-let weatherForecast=async ()=>{
-    let lat=37.983810;
-    let lon=37.983810;
-    let key='80d2ff5f959352f4319d73dc1f0171ce';
-    return  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`,
-        {method: 'GET'}
-    )
-        .then(response => {
-            return response.json();
-        })
-        .catch((error)=>{
-            return error;
-        });
+    let airPollution = async () => {
+        let lat = 37.983810;
+        let lon = 37.983810;
+        let key = '80d2ff5f959352f4319d73dc1f0171ce';
+        return fetch(`https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${key}`,
+            {method: 'GET'}
+        )
+            .then(response => {
+                return response.json();
+            })
+            .catch((error) => {
+                return error;
+            });
 
 
-}
+    }
+
+    let weatherForecast = async () => {
+        let lat = 37.983810;
+        let lon = 37.983810;
+        let key = '80d2ff5f959352f4319d73dc1f0171ce';
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`,
+            {method: 'GET'}
+        )
+            .then(response => {
+                return response.json();
+            })
+            .catch((error) => {
+                return error;
+            });
+    }
+
+    let findLocation = async (city_name) => {
+        let key = '80d2ff5f959352f4319d73dc1f0171ce';
+        let country_code = 'GR';
+        let limit = 1;
+        return fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city_name},${country_code}&limit=${limit}&appid=${key}`,
+            {method: 'GET'}
+        )
+            .then(response => {
+                return response.json();
+            })
+            .catch((error) => {
+                return error;
+            });
+    }
+    findLocation('Thessaloniki').then((response) => {
+        postMessage(JSON.stringify(response));
+    });
+
+
